949679717647d7353f7e51399e17c8d59a947d45
Tomas Bernotas <email@email.com>
2025-01-02 21:44:55 +0200

    Pridedamas js kodas

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const display = document.getElementById("display");
+    const buttons = document.querySelectorAll(".btn");
+  
+    let currentInput = "";
+    let previousInput = "";
+    let operator = null;
+
+});
